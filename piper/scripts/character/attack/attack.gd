46befd4544b157200d1106dcfc9d1768cc558daa
piperinnshall <piper.innshall@gmail.com>
2025-08-07 12:47:21 +1200

    chore: Add light and heavy attack

--- piper/scripts/character/attack/attack.gd
+++ piper/scripts/character/attack/attack.gd
@@ -7,4 +7,8 @@ func _ready():
 	monitoring = true
 
 func _on_life_span_timeout() -> void:
-	pass
+	queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	if body.has_method("_apply_damage"):
+		body.take_damage(damage)
